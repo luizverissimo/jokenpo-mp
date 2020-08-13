d2cb535f4c261922add7497325fd025bbc2c4b75
Luiz Verissimo <luizverissimosouza@gmail.com>
2020-08-13 20:20:25 -0300

    add images game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import imgRock from "./assets/rock.png";
+import imgPaper from "./assets/paper.png";
+import imgScissors from "./assets/scissors.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      style={{
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        className="cards"
+        style={{
+          display: "flex",
+        }}
+      >
+        <img
+          src={imgRock}
+          style={{ boxShadow: "3px 3px 5px 6px #ccc", margin: "30px" }}
+          alt="rock"
+          width="450"
+        />
+        <img
+          src={imgPaper}
+          style={{ boxShadow: "3px 3px 5px 6px #ccc", margin: "30px" }}
+          alt="paper"
+          width="450"
+        />
+        <img
+          src={imgScissors}
+          style={{ boxShadow: "3px 3px 5px 6px #ccc", margin: "30px" }}
+          alt="scissors"
+          width="450"
+        />
+      </div>
     </div>
   );
 }
